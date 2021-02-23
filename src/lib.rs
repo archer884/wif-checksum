1e3f3f2523a161186753bf3e2016244d38528219
J/A <archer884@gmail.com>
2021-02-23 13:47:24 -0600

    Undercomplicate things

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,8 @@ use sha2::{Digest, Sha256};
 
 pub type Result<T, E = base58::FromBase58Error> = std::result::Result<T, E>;
 
-pub trait HasBytes {
-    fn as_bytes(&self) -> &[u8];
-}
-
-impl HasBytes for [u8] {
-    fn as_bytes(&self) -> &[u8] {
-        todo!()
-    }
-}
-
-impl<T: AsRef<str>> HasBytes for T {
-    fn as_bytes(&self) -> &[u8] {
-        self.as_ref().as_bytes()
-    }
-}
-
-pub fn validate_checksum(key: &impl HasBytes) -> Result<bool> {
-    let key = key.as_bytes().from_base58()?;
+pub fn validate_checksum(key: &[u8]) -> Result<bool> {
+    let key = key.from_base58()?;
     let (key, checksum) = key.split_at(key.len() - 4);
     let a = hash(key);
     let b = hash(&a);
@@ -44,7 +28,7 @@ mod tests {
     #[test]
     fn validate_checksum_works() {
         // This is a real private key, but there is no money in it. Feel free to donate.
-        let test_key = "L2v6wDTptVRhwDTbK9XaJGBQ8Dbv5npMJwD4mJ7KwGYqS2ZFJptE";
-        assert!(validate_checksum(&test_key).unwrap_or_default());
+        let test_key = b"L2v6wDTptVRhwDTbK9XaJGBQ8Dbv5npMJwD4mJ7KwGYqS2ZFJptE";
+        assert!(validate_checksum(test_key).unwrap_or_default());
     }
 }
